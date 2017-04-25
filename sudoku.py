27cb579059fffd71b4134f74bc7cee09f3e70e60
Vincent Beraudier <vberaudi@users.noreply.github.com>
2017-04-25 10:43:53 +0200

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -8,14 +8,15 @@ problem_data = pd.read_csv("sudoku.csv", sep=";")
 problem = []
 for t in problem_data.itertuples(index=False):
     problem.append([i for i in t])
-print(problem)
     
 mdl = CpoModel(name="Sudoku")
 grid = [[integer_var(min=1, max=9, name="C" + str(l) + str(c)) for l in GRNG] for c in GRNG]
 for l in GRNG:
     mdl.add(all_diff([grid[l][c] for c in GRNG]))
+
 for c in GRNG:
     mdl.add(all_diff([grid[l][c] for l in GRNG]))
+
 ssrng = range(0, 9, 3)
 for sl in ssrng:
     for sc in ssrng:
