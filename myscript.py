2da8d8a41f03ae809b09eef66e1eb04d78924fe1
Vincent Beraudier <vincent.beraudier@fr.ibm.com>
2025-04-15 13:33:49 +0200

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -18,9 +18,9 @@ or a DOcplex model instance.
 """
 from docplex.mp.model_reader import ModelReader
 
-this is an error
 
-from docplex.mp.model_reader import XXX
+from docplex.mp.model_reader import MyModelRader
+from docplex.mp.odel_reader import ModelReader
 
 def populate_from_file(filename, gap=0.1,
                        pool_intensity=4,
